e1fa535722069b41cf79b52b0d48db2c36f2189c
Pedro Pachuca <pedritopachuca@me.com>
2017-03-31 14:25:58 -0700

    Initial Commit

---
+++ Desktop/JumbleSolverSwing/src/JumblePanel.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+
+public class JumblePanel extends JPanel {
+
+	LanguageMap map;
+	public JumblePanel(LanguageMap map) {
+		this.map = map;
+		this.setBackground(new Color(255, 128, 255));// just to make sure we can change...
+	}
+	
+	
+	
+}
