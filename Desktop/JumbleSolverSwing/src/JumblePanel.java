e6e9315fa83e6d60c80a14acaa98593c84f7f844
Pedro Pachuca <pedritopachuca@me.com>
2017-03-31 14:58:39 -0700

    Added Pull Down menu for word picking

--- Desktop/JumbleSolverSwing/src/JumblePanel.java
+++ Desktop/JumbleSolverSwing/src/JumblePanel.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
+import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 
 public class JumblePanel extends JPanel {
@@ -9,8 +12,23 @@ public class JumblePanel extends JPanel {
 	public JumblePanel(LanguageMap map) {
 		this.map = map;
 		this.setBackground(new Color(255, 128, 255));// just to make sure we can change...
+		createGUI();
+
 	}
-	
-	
-	
+	private void createGUI() {
+		JTextField myField = new JTextField();
+		myField.setText("Pick number of words");
+		myField.setEditable(false);
+		this.add(myField);
+		JComboBox<Integer> numberList = new JComboBox<Integer>();
+		for(int i = 1; i < 11; i++) {
+			numberList.addItem(i);
+		}
+		numberList.setSelectedIndex(0);
+		this.add(numberList);
+
+	}
+
+
+
 }
